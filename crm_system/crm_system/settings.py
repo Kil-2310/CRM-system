ce0e09b1aac42652d0313f9bd2860f1fbe76c88c
kil-2310 <homakove600@gmail.com>
2026-07-22 15:30:32 +0300

    Started working on new app

--- crm_system/crm_system/settings.py
+++ crm_system/crm_system/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'authentication.apps.AuthenticationConfig',
 ]
 
 MIDDLEWARE = [
