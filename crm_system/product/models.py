8e482a2f86c94d68a2244b09f43b65ad0534f576
kil-2310 <homakove600@gmail.com>
2026-07-27 11:23:26 +0300

    Updated module product

--- crm_system/product/models.py
+++ crm_system/product/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     """Модель продукта"""
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    cost = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    cost = models.DecimalField(max_digits=9, decimal_places=2, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
